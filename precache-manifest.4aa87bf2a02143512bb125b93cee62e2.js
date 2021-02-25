d6d415f04f2da64da6384e8d707f9711d6c51168
Jeremiah <newcreationjerry96@gmail.com>
2021-02-25 06:01:57 +0000

    Updates

---
+++ precache-manifest.4aa87bf2a02143512bb125b93cee62e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6b49c8599742cfffbd137c6be94853",
+    "url": "/Decentralized-Land-Registry/index.html"
+  },
+  {
+    "revision": "833cde9812debb542979",
+    "url": "/Decentralized-Land-Registry/static/css/main.eafac940.chunk.css"
+  },
+  {
+    "revision": "8d9993ca3189279a7a5e",
+    "url": "/Decentralized-Land-Registry/static/js/2.2c7156ae.chunk.js"
+  },
+  {
+    "revision": "833cde9812debb542979",
+    "url": "/Decentralized-Land-Registry/static/js/main.e29108df.chunk.js"
+  },
+  {
+    "revision": "cc1f212524317d476b61",
+    "url": "/Decentralized-Land-Registry/static/js/runtime-main.70e5f91e.js"
+  },
+  {
+    "revision": "0c17a4fbad7350c320f18bf98776ed3f",
+    "url": "/Decentralized-Land-Registry/static/media/darklogo.0c17a4fb.svg"
+  }
+]);
